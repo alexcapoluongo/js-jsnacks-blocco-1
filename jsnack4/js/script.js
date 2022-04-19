e83c53d4336420fc770bb2348fed01b06dde3f9e
alexcapoluongo <alexcapoluongo@icloud.com>
2022-04-19 15:04:06 +0200

    jsnack4 first method added

---
+++ jsnack4/js/script.js
@@ -0,0 +1,25 @@
+// jsnack4. Chiedi un numero di 4 cifre all'utente
+// e calcola la somma di tutte le cifre che compongono il numero.
+
+// Chiedi un numero di 4 cifre all'utente
+// let userNumber = (prompt('inserisci un numero di 4 cifre'));
+// console.log(userNumber);
+
+// let number1 = parseInt(userNumber[0]);
+// let number2 = parseInt (userNumber[1]);
+// let number3 = parseInt (userNumber[2]);
+// let number4 = parseInt (userNumber[3]);
+
+// console.log(number1 + number2 + number3 + number4);
+
+// ----
+let userNumber = (prompt('inserisci un numero di 4 cifre'));
+console.log(userNumber);
+let number = parseInt(userNumber);
+
+for (let i = 0; i < userNumber.length - 1; i++){
+    let numberOne = number[0];
+}
+
+console.log(number);
+console.log(numberOne);
